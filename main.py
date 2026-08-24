3245755149bad1f68af9dc00e24f0521e4b162ae
aimee <aimeestone123@gmail.com>
2026-08-24 14:36:36 +0100

    Refactor ffmpeg check in main function and update error messages

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import os
 import sys
 import subprocess
 import yt_dlp
 
 
-def has_ytdlp() -> bool:
+def has_ffmpeg() -> bool:
     try:
-        subprocess.run(['yt-dlp', '-h'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['ffmpeg', '-h'], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         return True
-    except:
+    except (subprocess.CalledProcessError, FileNotFoundError):
         return False
 
 
 def main():
-    if has_ytdlp() is False:
-        print("yt-dlp isn't installed on your system, to install it run 'winget install yt-dlp'.")
+    if has_ffmpeg() is False:
+        print("ffmpeg isn't installed on your system, to install it run 'winget install ffmpeg' then restart your terminal.")
         sys.exit()
 
     url = input("Enter the youtube URL to download [q to quit]: ")
@@ -27,18 +26,22 @@ def main():
         'format': 'bestaudio/best',
         'restrictfilenames': True,
         'windowsfilenames': True,
+        'quiet': True,
+        'no_warnings': True,
+        'noplaylist': True,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
         }]
     }
-    
+
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             print(f"File saved to: {info['requested_downloads'][0]['filepath']}")
     except yt_dlp.utils.DownloadError:
         print("This video might be private or geo-blocked so you can't download :(")
+        sys.exit()
 
 
 if __name__ == "__main__":
